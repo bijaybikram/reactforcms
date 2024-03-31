e21307c04f68d5f3314f97c6b6497e4837043d40
Bijay Bikram <becauseiam33@gmail.com>
2024-03-31 14:09:28 +0545

    an approach to post form data using formData

--- src/pages/CreateBlog/CreateBlog.jsx
+++ src/pages/CreateBlog/CreateBlog.jsx
@@ -6,23 +6,32 @@ import { useNavigate } from 'react-router-dom'
 
 const CreateBlog = () => {
   const navigate = useNavigate()
-
-  const [title, setTitle] = useState("")
-  const [subTitle, setSubTitle] = useState("")
-  const [description, setDescription] = useState("")
   
   const createBlog = async (e) => {
     e.preventDefault()
-    const data = {
-      title: title,
-      subTitle: subTitle,
-      description: description
-    }
+
+    // Predefined react method to extract form input datas in object using DOM formdata 
+    const formData = new FormData(e.currentTarget)
+    // FIRST APPROACH
+    // console.log(formData.get('title'))
+    // const data = {
+    //   title: formData.get("title"),
+    //   subTitle: formData.get("subTitle"),
+    //   description: formData.get("description"),
+    // }
+
+    // SECOND APPROACH
+    // changing the given formData data format from array to key value object notation and inserting them on data variable
+    const data = Object.fromEntries(formData)
+
+    
     // send above states data to API
     const response = await axios.post("http://localhost:3000/blogs", data)
     if (response.status == 201) {
-      // alert(response.data.message)
+      alert(response.data.message)
       navigate("/")
+    }else{
+      alert("Something went wrong!")
     }
     
   }
@@ -33,13 +42,13 @@ const CreateBlog = () => {
       <form onSubmit={createBlog}>
         <h2>Create a Blog Post</h2>
         <label htmlFor="title">Title:</label>
-        <input type="text" id="title" name="title" placeholder="Enter title" onChange={(e)=>setTitle(e.target.value)}/>
+        <input type="text" id="title" name="title" placeholder="Enter title" />
         
         <label htmlFor="subTitle">Subtitle:</label>
-        <input type="text" id="sub-title" name="subTitle" placeholder="Enter subtitle" onChange={(e)=>setSubTitle(e.target.value)}/>
+        <input type="text" id="sub-title" name="subTitle" placeholder="Enter subtitle" />
         
         <label htmlFor="description">Description:</label>
-        <textarea id="content" name="description" placeholder="Enter blog content" rows="6" onChange={(e)=>setDescription(e.target.value)}></textarea>
+        <textarea id="content" name="description" placeholder="Enter blog content" rows="6" ></textarea>
         
         <input type="submit" value="Submit"/>
       </form>
