6465decad964b719ae9ef17d1a7319a9c6da3821
Bijay Bikram <becauseiam33@gmail.com>
2024-04-01 23:48:35 +0545

    blog deletion implemented

--- src/pages/SingleBlog/SingleBlog.jsx
+++ src/pages/SingleBlog/SingleBlog.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios'
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
+import Button from '../../components/Button/Button'
 
 const SingleBlog = () => {
+  const navigate = useNavigate()
   const {id} = useParams()
   const [blog, setBlog] = useState({})
   // console.log(id)
@@ -15,6 +17,18 @@ const SingleBlog = () => {
     }
   }
   // console.log(blog)
+  // deleting blog
+  const deleteBlog = async () => {
+    try{
+      const response = await axios.delete('http://localhost:3000/blogs/'+ id)
+      if(response.status == 200){
+        navigate('/')
+      }
+    }catch(error){
+      console.log("something went wrong!")
+    }
+  }
+
 
   useEffect(() => {
     fetchSingleBlog()
@@ -28,6 +42,7 @@ const SingleBlog = () => {
             <h2 className="card-title" >{blog.title}</h2> 
             <h5 className="card-subtitle" >{blog.subTitle}</h5> 
             <p className="card-description">{blog.description}</p>
+            <Button onClick = {deleteBlog} name="Delete"/>
           </div>
         </div>
     </>
